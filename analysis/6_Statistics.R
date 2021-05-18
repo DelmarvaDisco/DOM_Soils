da40a49cf832e216925e4037259418efe9e03368
Wardinski <wardinskik@vt.edu>
2021-05-18 15:50:35 -0400

    Adding to basic statistical analysis

--- analysis/6_Statistics.R
+++ analysis/6_Statistics.R
@@ -24,33 +24,40 @@ library(ggpmisc)
 
 #Read data
 df<-read_csv("data/R_Extraction_Results_All.csv")
-annual <- read_csv("data/annual_metrics.csv")
+annual <- read_csv("data/annual_metrics_2020.csv")
+waterlevel <- read_csv("data/waterLevel_at_sampling_location.csv")
 
 #Filter to just wetlands and no leaf litter
 Wetlands <- df %>% filter(wetland %in% c("QB","TB","DB","ND"))
 WetlandsNoLL <- Wetlands %>% filter(Point != "5 LL")
 
-#Join data
+#Join metrics and ESOM data
 data <- inner_join(df, annual, by=c("wetland","station"))
+
+#Filter water level data to just 2020 water year
+waterlevel <- waterlevel %>% filter(Timestamp > "2019-10-01" & Timestamp < "2020-10-01")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #2.0 Exploratory Data Analysis--------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#Look at distribution
-#EOC
+#Look at distributions/normality/equal variance 
+
+### 2.1 EOC -----------------------------------
 hist(WetlandsNoLL$EOC_mgC_L)
 qqnorm(WetlandsNoLL$EOC_mgC_L)
 shapiro.test(WetlandsNoLL$EOC_mgC_L) #EOC values are not normally distributed
 
 #Log transform data
 #EOC
-logEOC <- log(WetlandsNoLL$EOC_mgC_L)
+logEOC <- log10(WetlandsNoLL$EOC_mgC_L)
 qqnorm(logEOC)
+
 #test for normal distribution of log transformed data
 shapiro.test(logEOC) #Log of values is normally distributed
 Station <- WetlandsNoLL$station
 Horizon <- WetlandsNoLL$Generic_Horizon
+
 #test for equal variance
 bartlett.test(logEOC~Station) #Barlett test doesn't meet equal variance assumptions
 bartlett.test(logEOC~Horizon) 
@@ -58,62 +65,81 @@ leveneTest(logEOC,Station,center=median) #Levene test doesn't meet equal varianc
 leveneTest(logEOC,Horizon,center=median)
 
 
-#FI
+### 2.2 FI -----------------------------------
 qqnorm(df$FI)
 shapiro.test(df$FI)
 logFI <- log(df$FI)
 qqnorm(logFI)
 shapiro.test(logFI) #logging FI data is not normally distributed
 
+### 2.3 Water Level Data -----------------------------------
+#break out variables
+y_n <- waterlevel$y_n
+station <- waterlevel$station
+
+#distribution and normality test
+hist(y_n) #looks normally distributed overall
+qqnorm(y_n) #ends of plot are wonky but overall looks ok
+shapiro.test(y_n) #shapiro doesn't work when n>5,000 :(
+
+#test for equal variance
+bartlett.test(y_n~station) #big fail p = 9.4x10^-5
+leveneTest(y_n,station,center=median) #p = 5.97x10-7
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#3.0 Linear Regression --------------------------------------------------
+#3.0 ANOVA/TukeyHSD --------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#Using fake water level data for practice
-#Mean WL
+### 3.1 EOC --------------------------------------------------
+#4.1.1 EOC by horizon------------------------------------------
+logEOC.horiz.aov <- aov(logEOC~Horizon)
+summary(logEOC.horiz.aov)
+logEOC.horiz.HSD <- TukeyHSD(logEOC.horiz.aov);logEOC.horiz.HSD 
+#one-way anova test for unequal variance
+oneway.test(logEOC~Horizon)
+
+#3.1.2 EOC by station------------------------------------------
+logEOC.sta.aov <- aov(logEOC~Station)
+summary(logEOC.sta.aov)
+logEOC.sta.HSD <- TukeyHSD(logEOC.sta.aov);logEOC.sta.HSD 
+#one-way anova test for unequal variance
+oneway.test(logEOC~Station)
+
+#3.1.3 EOC by both horizon and transect location (ANOCVA) --------------------
+logEOC.both.aov <- aov(logEOC~Horizon+Station)
+summary(logEOC.both.aov)
+logEOC.both.HSD <- TukeyHSD(logEOC.both.aov);logEOC.both.HSD  
+
+### 3.2 Water Level Data--------------------------------
+waterlevel.aov <- aov(y_n~station)
+summary(waterlevel.aov)
+waterlevel.HSD <- TukeyHSD(waterlevel.aov);waterlevel.HSD 
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#4.0 Kruskal-Wallis --------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+###4.1 EOC --------------------------------------------------
 #EOC by horizon
-my.formula <- data$EOC_mgC_L~data$mean_waterLevel
-ggplot(data, aes(mean_waterLevel,EOC_mgC_L,col=Generic_Horizon)) +
-  geom_point(size=2.5) +
-  xlab("Mean Water Level (m)") +
-  ylab("EOC (mg/L)") + 
-  ggtitle("Wetland EOC vs Mean WL") + 
-  theme_bw()+
-  geom_smooth(method = "lm")
-  #stat_poly_eq(formula=my.formula,
-               #aes(label=paste(..eq.label..,..rr.label..,sep=="~~~")), parse=TRUE )
-#log data
-ggplot(data, aes(mean_waterLevel,log(EOC_mgC_L),col=Generic_Horizon)) +
-  geom_point(size=2.5) +
-  xlab("Mean Water Level (m)") +
-  ylab("log(EOC) (mg/L)") + 
-  ggtitle("Wetland EOC vs Mean WL") + 
-  theme_bw()+
-  geom_smooth(method = 'lm')
+kruskal.test(logEOC~Horizon)
+kruskalmc(logEOC,Horizon,probs=0.05)
+#EOC by station
+kruskal.test(logEOC~Station)
+kruskalmc(logEOC,Station,probs=0.05)
 
-#all data
-ggplot(data, aes(mean_waterLevel,EOC_mgC_L)) +
-  geom_point(size=2.5) +
-  xlab("Mean Water Level (m)") +
-  ylab("EOC (mg/L)") + 
-  ggtitle("Wetland EOC vs Mean WL") + 
-  theme_bw()+
-  geom_smooth(method = 'lm')
-#log data
-ggplot(data, aes(mean_waterLevel,log(EOC_mgC_L))) +
-  geom_point(size=2.5) +
-  xlab("Mean Water Level (m)") +
-  ylab("log(EOC) (mg/L)") + 
-  ggtitle("Wetland EOC vs Mean WL") + 
-  theme_bw()+
-  geom_smooth(method = 'lm')
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#5.0 Linear Regression --------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#Separate horizons
 O <- data %>% filter(Generic_Horizon == "1O")
 A <- data %>% filter(Generic_Horizon == "2A")
 B <- data %>% filter(Generic_Horizon == "3B")
 
-#untransformed
+### 5.1 Mean WL ------------------------------
+
+# 5.1.1 EOC ------------------------------
 lmEOCO <- lm(O$EOC_mgC_L~O$mean_waterLevel)
 summary(lmEOCO)
 lmEOCA <- lm(A$EOC_mgC_L~A$mean_waterLevel)
@@ -123,92 +149,30 @@ summary(lmEOCB)
 lmoverall <- lm(data$EOC_mgC_L~data$mean_waterLevel)
 summary(lmoverall)
 
-#plot using base R
-plot(O$EOC_mgC_L~O$mean_depth_m)
-points(A$EOC_mgC_L~A$mean_depth_m,col="red")
-points(B$EOC_mgC_L~B$mean_depth_m,col="blue")
-abline(lmEOCO)
-abline(lmEOCA,col="red")
-abline(lmEOCB,col="blue")
+# 5.1.2 SUVA ------------------------------
+lmSUVAO <- lm(O$SUVA254_L_mgm~O$mean_waterLevel)
+summary(lmSUVAO)
+lmSUVAA <- lm(A$SUVA254_L_mgm~A$mean_waterLevel)
+summary(lmSUVAA)
+lmSUVAB <- lm(B$SUVA254_L_mgm~B$mean_waterLevel)
+summary(lmSUVAB)
+lmoverall <- lm(data$SUVA254_L_mgm~data$mean_waterLevel)
+summary(lmoverall)
 
-#logtransformed
-lmlogEOCO <- lm(O$mean_depth_m~log(O$EOC_mgC_L))
-summary(lmlogEOCO)
-lmlogEOCA <- lm(A$mean_depth_m~log(A$EOC_mgC_L))
-summary(lmlogEOCA)
-lmlogEOCB <- lm(B$mean_depth_m~log(B$EOC_mgC_L))
-summary(lmlogEOCB)
-lmlogoverall <- lm(data$mean_depth_m~log(data$EOC_mgC_L))
-summary(lmlogoverall)
+# 5.1.3 HIX ------------------------------
+lmoverall <- lm(data$HIX~data$mean_waterLevel)
+summary(lmoverall)
 
-#N_events
-#FI
-ggplot(data, aes(n_events,FI,col=Generic_Horizon)) +
-  geom_point(size=2.5) +
-  xlab("Number of saturation events") +
-  ylab("FI") + 
-  theme_bw()+
-  geom_smooth(method = 'lm')
+### 5.2 N_events -----------------------------
 
-ggplot(data, aes(n_events,FI)) +
-  geom_point(size=2.5) +
-  xlab("Number of saturation events") +
-  ylab("FI") + 
-  theme_bw()+
-  geom_smooth(method = 'lm')
 
-lmFIO <- lm(O$n_events~O$FI)
+### 5.3 Duration -----------------------------
+
+lmFIO <- lm(O$FI~O$dur_day)
 summary(lmFIO)
-lmFIA <- lm(A$n_events~A$FI)
+lmFIA <- lm(A$FI~A$dur_day)
 summary(lmFIA)
-lmFIB <- lm(B$n_events~B$FI)
+lmFIB <- lm(B$FI~B$dur_day)
 summary(lmFIB)
-lmFIoverall <-(data$n_events~data$FI)
+lmFIoverall <-(data$FI~data$dur_day)
 summary(lmFIoverall)
-
-#min water level
-#EOC by horizon
-ggplot(data, aes(min_depth_m,EOC_mgC_L,col=Generic_Horizon)) +
-  geom_point(size=2.5) +
-  xlab("Minimum Depth to Water Table (m)") +
-  ylab("EOC (mg/L)") + 
-  ggtitle("Wetland EOC vs Mean WL") + 
-  theme_bw()+
-  geom_smooth(method = 'lm')
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#4.0 ANOVA --------------------------------------------------
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#EOC by horizon
-logEOC.horiz.aov <- aov(logEOC~Horizon)
-summary(logEOC.horiz.aov)
-logEOC.horiz.HSD <- TukeyHSD(logEOC.horiz.aov);logEOC.horiz.HSD 
-
-#one-way anova test for unequal variance
-oneway.test(logEOC~Horizon)
-
-#EOC by station
-logEOC.sta.aov <- aov(logEOC~Station)
-summary(logEOC.sta.aov)
-logEOC.sta.HSD <- TukeyHSD(logEOC.sta.aov);logEOC.sta.HSD 
-
-#one-way anova test for unequal variance
-oneway.test(logEOC~Station)
-
-#EOC by both horizon and transect location
-logEOC.both.aov <- aov(logEOC~Horizon+Station)
-summary(logEOC.both.aov)
-logEOC.both.HSD <- TukeyHSD(logEOC.both.aov);logEOC.both.HSD  
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#5.0 Kruskal-Wallis --------------------------------------------------
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#EOC by horizon
-kruskal.test(logEOC~Horizon)
-kruskalmc(logEOC,Horizon,probs=0.05)
-#EOC by station
-kruskal.test(logEOC~Station)
-kruskalmc(logEOC,Station,probs=0.05)
-
