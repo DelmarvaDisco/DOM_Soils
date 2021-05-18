763890bc1badb0bb5842999e62f799fbf3824ff3
Wardinski <wardinskik@vt.edu>
2021-05-18 12:38:53 -0400

    Added more plots

--- analysis/5_ESOM_and_HydroMetrics.R
+++ analysis/5_ESOM_and_HydroMetrics.R
@@ -70,8 +70,11 @@ maxEOC <- ggplot(data, aes(max_waterLevel,EOC_mgC_L,col=Generic_Horizon)) +
   theme_bw()+
   geom_smooth(method = 'lm') 
 
-#2.1.4 Plot
-figureEOC <- ggarrange(mstat, meanEOC, minEOC, maxEOC,
+#2.1.4 EOC Plot
+figureEOC <- ggarrange(mstat, 
+                       meanEOC, 
+                       minEOC, 
+                       maxEOC,
                     labels = c("A", "B", "C","D"),
                     ncol = 2, nrow = 2)
 figureEOC
@@ -112,8 +115,11 @@ FImax <- ggplot(data, aes(min_waterLevel,FI,col=Generic_Horizon)) +
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#2.2.4 Plot---------------------------------------
-figureFI <- ggarrange(FIoverall,FImean, FImin, FImax,
+#2.2.4 FI Plot---------------------------------------
+figureFI <- ggarrange(FIoverall,
+                      FImean, 
+                      FImin, 
+                      FImax,
                        labels = c("A", "B", "C","D"),
                        ncol = 2, nrow = 2)
 figureFI
@@ -180,8 +186,11 @@ SUVAmax <- ggplot(data, aes(max_waterLevel,SUVA254_L_mgm,col=Generic_Horizon)) +
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#2.3.4 Plot---------------------------------------
-figureSUVA <- ggarrange(SUVAoverall,SUVAmean, SUVAmin, SUVAmax,
+#2.3.4 SUVA Plot---------------------------------------
+figureSUVA <- ggarrange(SUVAoverall,
+                        SUVAmean, 
+                        SUVAmin, 
+                        SUVAmax,
                       labels = c("A", "B", "C","D"),
                       ncol = 2, nrow = 2)
 figureSUVA
@@ -224,8 +233,11 @@ HIXmax <- ggplot(data, aes(max_waterLevel,HIX,col=Generic_Horizon)) +
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#2.4.4 Plot ---------------------------------------
-figureHIX <- ggarrange(HIXoverall,HIXmean, HIXmin, HIXmax,
+#2.4.4 HIX Plot ---------------------------------------
+figureHIX <- ggarrange(HIXoverall,
+                       HIXmean, 
+                       HIXmin, 
+                       HIXmax,
                         labels = c("A", "B", "C","D"),
                         ncol = 2, nrow = 2)
 figureHIX
@@ -247,13 +259,13 @@ SSRmean <- ggplot(data, aes(mean_waterLevel,SSR,col=Generic_Horizon)) +
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#2.5.2 Plot---------------------------------------
+#2.5.2 SSR Plot---------------------------------------
 figureSSR <- ggarrange(SSRoverall,SSRmean,
                        labels = c("A", "B"),
                        ncol = 1, nrow = 2)
 figureSSR
 
-### 2.6 %Protein ### ---------------------------------------
+### 2.6 %Protein ###---------------------------------------
 #2.6.1 %P vs Mean WL ---------------------------------------
 Poverall <- ggplot(data, aes(mean_waterLevel,Percent_Protein)) +
   geom_point(size=2.5) +
@@ -270,19 +282,20 @@ Pmean <- ggplot(data, aes(mean_waterLevel,Percent_Protein,col=Generic_Horizon)) 
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#2.6.2 Plot---------------------------------------
+#2.6.2 Protein Plot---------------------------------------
 figureP <- ggarrange(Poverall,Pmean,
                        labels = c("A", "B"),
                        ncol = 1, nrow = 2)
 figureP
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#3.0 Duration Inundation and ESOM ----------------------------------------------
+#3.0 Duration Inundation, Wet/Dry Alternations, % of year Saturated  -----------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#EOC
-#by horizon
-ggplot(data, aes(dur_day,EOC_mgC_L,col=Generic_Horizon)) +
+### 3.1 EOC ###---------------------------------------
+#3.1.1 EOC vs Dur ---------------------------------------
+#horizon
+EOCdur <- ggplot(data, aes(dur_day,EOC_mgC_L,col=Generic_Horizon)) +
   geom_point(size=2.5) +
   xlab("Duration (d)") +
   ylab("EOC (mg/L)") + 
@@ -298,64 +311,25 @@ ggplot(data, aes(dur_day,EOC_mgC_L)) +
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#FI
-ggplot(data, aes(dur_day,FI,col=Generic_Horizon)) +
+#3.1.2 EOC vs N events ---------------------------------------
+EOCn <- ggplot(data, aes(n_events,EOC_mgC_L,col=Generic_Horizon)) +
   geom_point(size=2.5) +
-  xlab("Duration (d)") +
-  ylab("FI") + 
-  ggtitle("Wetland FI vs Duration") + 
-  theme_bw()
-
-#SUVA
-ggplot(data, aes(dur_day,SUVA254_L_mgm,col=Generic_Horizon)) +
-  geom_point(size=2.5) +
-  xlab("Duration (d)") +
-  ylab("SUVA (L/mg-m)") + 
-  ggtitle("Wetland SUVA vs Duration") + 
+  xlab("n events") +
+  ylab("EOC (mg/L)") + 
+  ggtitle("Wetland EOC vs N Saturation Events") + 
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#4.0 # Alternations Wet/Dry and ESOM -------------------------------------------
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#EOC
-ggplot(data, aes(n_events,EOC_mgC_L,col=Generic_Horizon)) +
+ggplot(data, aes(n_events,EOC_mgC_L)) +
   geom_point(size=2.5) +
   xlab("n events") +
   ylab("EOC (mg/L)") + 
   ggtitle("Wetland EOC vs N Saturation Events") + 
-  theme_bw()
+  theme_bw() +
+  geom_smooth(method = 'lm')
 
-ggplot(data, aes(n_events,EOC_mgC_L,col=station)) +
-  geom_point(size=2.5) +
-  xlab("n events") +
-  ylab("EOC (mg/L)") + 
-  ggtitle("Wetland EOC vs N Saturation Events") + 
-  theme_bw()
-
-#FI
-ggplot(data, aes(n_events,FI,col=Generic_Horizon)) +
-  geom_point(size=2.5) +
-  xlab("n events") +
-  ylab("FI") + 
-  ggtitle("Wetland FI vs N Saturation Events") + 
-  theme_bw()
-
-#SUVA
-ggplot(data, aes(n_events,SUVA254_L_mgm,col=Generic_Horizon)) +
-  geom_point(size=2.5) +
-  xlab("n events)") +
-  ylab("SUVA (L/mg-m)") + 
-  ggtitle("Wetland SUVA vs N Saturation Events") + 
-  theme_bw()
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#5.0 Percent of year saturated -------------------------------------------
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#EOC
-ggplot(data, aes(percent_sat,EOC_mgC_L,col=Generic_Horizon)) +
+#3.1.3 % Yr Sat ---------------------------------------
+EOCsat <- ggplot(data, aes(percent_sat,EOC_mgC_L,col=Generic_Horizon)) +
   geom_point(size=2.5) +
   xlab("% of year saturated") +
   ylab("EOC (mg/L)") + 
@@ -371,18 +345,258 @@ ggplot(data, aes(n_events,EOC_mgC_L)) +
   theme_bw()+
   geom_smooth(method = 'lm')
 
-#FI
-ggplot(data, aes(n_events,FI,col=Generic_Horizon)) +
+#3.1.4 CV of water level ---------------------------------------
+EOCcv <- ggplot(data, aes(CV_waterLevel,EOC_mgC_L,col=Generic_Horizon)) +
   geom_point(size=2.5) +
-  xlab("% of year saturated") +
-  ylab("FI") + 
-  ggtitle("Wetland FI vs % of year saturated") + 
-  theme_bw()
+  xlab("CV of Water Level") +
+  ylab("EOC (mg/L)") + 
+  ggtitle("Wetland EOC vs CV") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
 
-#SUVA
-ggplot(data, aes(n_events,SUVA254_L_mgm,col=Generic_Horizon)) +
+
+#3.1.4 EOC Plot ---------------------------------------
+
+figureEOC <- ggarrange(EOCdur, 
+                       EOCsat, 
+                       EOCn, 
+                       EOCcv,
+                     labels = c("A", "B","C","D"),
+                     ncol = 2, nrow = 2)
+figureEOC
+
+
+### 3.2 FI ###---------------------------------------
+#3.2.1 FI vs Dur ---------------------------------------
+FIdur<- ggplot(data, aes(dur_day,FI,col=Generic_Horizon)) +
   geom_point(size=2.5) +
-  xlab("% of year saturated)") +
+  xlab("Duration (d)") +
+  ylab("FI") + 
+  ggtitle("Wetland FI vs Duration") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.2.2 FI vs N events -----------------------------------
+FIn <- ggplot(data, aes(n_events,FI,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("n events") +
+  ylab("FI") + 
+  ggtitle("Wetland FI vs N Saturation Events") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.2.3 % Yr Sat ---------------------------------------
+FIsat <- ggplot(data, aes(percent_sat,FI,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("% of Year Saturated") +
+  ylab("FI") + 
+  ggtitle("Wetland FI vs % of Year Saturated") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.2.4 CV of water level --------------------------------
+FIcv <- ggplot(data, aes(CV_waterLevel,FI,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("CV of Water Level") +
+  ylab("FI") + 
+  ggtitle("Wetland FI vs CV of Water Level") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.2.5 FI Plot ---------------------------------------
+figureFI <- ggarrange(FIdur, 
+                      FIsat, 
+                      FIn, 
+                      FIcv,
+                      labels = c("A", "B","C","D"),
+                      ncol = 2, nrow = 2)
+
+figureFI
+
+### 3.3 SUVA ###--------------------------------------- 
+#3.2.1 SUVA vs Dur ---------------------------------------
+SUVAdur <- ggplot(data, aes(dur_day,SUVA254_L_mgm,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("Duration (d)") +
   ylab("SUVA (L/mg-m)") + 
-  ggtitle("Wetland SUVA vs % of year saturated") + 
-  theme_bw()
+  ggtitle("Wetland SUVA vs Duration") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+
+#3.3.2 SUVA vs N events -----------------------------------
+SUVAn <- ggplot(data, aes(n_events,SUVA254_L_mgm,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("n events") +
+  ylab("SUVA (L/mg-m)") + 
+  ggtitle("Wetland SUVA vs N Saturation Events") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.3.3 % Yr Sat ---------------------------------------
+SUVAsat <- ggplot(data, aes(percent_sat,SUVA254_L_mgm,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("% of Year Saturated") +
+  ylab("SUVA (L/mg-m)") + 
+  ggtitle("Wetland SUVA vs % of Year Saturated") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.3.4 CV of water level --------------------------------
+SUVAcv <- ggplot(data, aes(CV_waterLevel,SUVA254_L_mgm,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("CV of Water Level") +
+  ylab("SUVA (L/mg-m)") + 
+  ggtitle("Wetland SUVA vs Water Level CV") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.3.5 SUVA Plot ---------------------------------------
+figureSUVA <- ggarrange(SUVAdur, 
+                        SUVAsat, 
+                        SUVAn, 
+                        SUVAcv,
+                        labels = c("A", "B","C","D"),
+                        ncol = 2, nrow = 2)
+figureSUVA
+
+### 3.4 HIX ###---------------------------------------
+#3.4.1 HIX vs Dur ---------------------------------------
+HIXdur <- ggplot(data, aes(dur_day,HIX,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("Duration (d)") +
+  ylab("HIX") + 
+  ggtitle("Wetland HIX vs Duration") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+
+#3.4.2 HIXvs N events -----------------------------------
+HIXn <- ggplot(data, aes(n_events,HIX,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("n events") +
+  ylab("HIX") + 
+  ggtitle("Wetland HIX vs N Saturation Events") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.4.3 % Yr Sat ---------------------------------------
+HIXsat <- ggplot(data, aes(percent_sat,HIX,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("% of Year Saturated") +
+  ylab("HIX") + 
+  ggtitle("Wetland HIX vs % of Year Saturated") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.4.4 CV of water level --------------------------------
+HIXcv <- ggplot(data, aes(CV_waterLevel,HIX,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("CV of Water Level") +
+  ylab("HIX") + 
+  ggtitle("Wetland HIX vs Water Level CV") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.4.5 HIX Plot ---------------------------------------
+figureHIX <- ggarrange( HIXdur, 
+                        HIXsat, 
+                        HIXn, 
+                        HIXcv,
+                        labels = c("A", "B","C","D"),
+                        ncol = 2, nrow = 2)
+figureHIX
+
+### 3.5 SSR ###---------------------------------------
+#3.5.1 SSR vs Dur ---------------------------------------
+SSRdur <- ggplot(data, aes(dur_day,SSR,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("Duration (d)") +
+  ylab("SSR") + 
+  ggtitle("Wetland SSR vs Duration") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+
+#3.5.2 SSR vs N events -----------------------------------
+SSRn <- ggplot(data, aes(n_events,SSR,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("n events") +
+  ylab("SSR") + 
+  ggtitle("Wetland SSR vs N Saturation Events") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.5.3 % Yr Sat ---------------------------------------
+SSRsat <- ggplot(data, aes(percent_sat,SSR,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("% of Year Saturated") +
+  ylab("SSR") + 
+  ggtitle("Wetland SSR vs % of Year Saturated") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.5.4 CV of water level --------------------------------
+SSRcv <- ggplot(data, aes(CV_waterLevel,SSR,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("CV of Water Level") +
+  ylab("SSR") + 
+  ggtitle("Wetland SSR vs Water Level CV") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.5.5 SSR Plot ---------------------------------------
+figureSSR <- ggarrange( SSRdur, 
+                        SSRsat, 
+                        SSRn, 
+                        SSRcv,
+                        labels = c("A", "B","C","D"),
+                        ncol = 2, nrow = 2)
+figureSSR
+
+### 3.6 %Protein ###---------------------------------------
+#3.6.1 % Protein vs Dur ---------------------------------------
+Pdur <- ggplot(data, aes(dur_day,Percent_Protein,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("Duration (d)") +
+  ylab("% Protein") + 
+  ggtitle("Wetland % Protein vs Duration") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+
+#3.6.2 % Protein vs N events -----------------------------------
+Pn <- ggplot(data, aes(n_events,Percent_Protein,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("n events") +
+  ylab("% Protein") + 
+  ggtitle("Wetland % Protein vs N Saturation Events") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.6.3 % Yr Sat ---------------------------------------
+Psat <- ggplot(data, aes(percent_sat,Percent_Protein,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("% of Year Saturated") +
+  ylab("% Protein") + 
+  ggtitle("Wetland % Protein vs % of Year Saturated") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.6.4 CV of water level --------------------------------
+Pcv <- ggplot(data, aes(CV_waterLevel,Percent_Protein,col=Generic_Horizon)) +
+  geom_point(size=2.5) +
+  xlab("CV of Water Level") +
+  ylab("% Protein") + 
+  ggtitle("Wetland % Protein vs Water Level CV") + 
+  theme_bw()+
+  geom_smooth(method = 'lm')
+
+#3.6.5 % Protein Plot ---------------------------------------
+figureP <- ggarrange( Pdur, 
+                      Psat, 
+                      Pn, 
+                      Pcv,
+                      labels = c("A", "B","C","D"),
+                      ncol = 2, nrow = 2)
+figureP
